02cc96fe1d6341af6682e0c920042690aad85c67
Leonidez rosado <2020152397@ub.edu.bz>
2023-02-13 13:14:33 -0600

    learned waitgroups

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LeonidezRosado/waitgroup
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,34 @@
+// Filename: main.go
+//"waitgroups" are just another way to syncronize your goroutines
+package main
+
+import (
+	"fmt"
+	"sync"
+	//"time"
+)
+
+func one (wg *sync.WaitGroup)  {
+	defer wg.Done()
+	fmt.Println("hola")
+}
+func two (wg *sync.WaitGroup)  {
+	defer wg.Done()
+	fmt.Println("ni hao")
+}
+func three (wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("hello")
+}
+
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go one(&wg)
+	go two(&wg)
+	go three(&wg)
+	//
+	wg.Wait()
+}
