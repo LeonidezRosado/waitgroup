61f7d758e10967f3f57ef5e43012af82994bc3da
Leonidez rosado <2020152397@ub.edu.bz>
2023-02-13 13:48:03 -0600

    flags

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 // Filename: main.go
-//"waitgroups" are just another way to syncronize your goroutines
+// "waitgroups" are just another way to syncronize your goroutines
 package main
 
+//demonstrate flags
 import (
+	"flag"
 	"fmt"
-	"sync"
-	//"time"
+	"strings"
 )
 
-func one (wg *sync.WaitGroup)  {
+//"fmt"
+//"sync"
+//"time"
+
+/*func one (wg *sync.WaitGroup)  {
 	defer wg.Done()
 	fmt.Println("hola")
 }
@@ -20,15 +25,28 @@ func three (wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("hello")
 }
-
+*/
 
 func main() {
 
-	var wg sync.WaitGroup
+	/*var wg sync.WaitGroup
 	wg.Add(3)
 	go one(&wg)
 	go two(&wg)
 	go three(&wg)
 	//
-	wg.Wait()
+	wg.Wait()*/
+
+	//set the flags
+	msg := flag.String("msg", "Howdy, stranger!", "the message to display")
+	num := flag.Int("num",1 , "how many time to display the message")
+	caps := flag.Bool("caps", false,"should the string print in caps")
+	flag.Parse()
+	//check if the user set the flag
+	//fmt.Println(*msg)
+	//fmt.Println(*num)
+	
+	for i := 0; i < *num; i++ {
+		fmt.Println(*msg)
+	}
 }
