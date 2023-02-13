03b4154269ebd2621a4976bb2b7fd576271099c1
Leonidez rosado <2020152397@ub.edu.bz>
2023-02-13 13:49:18 -0600

    flags2

--- main.go
+++ main.go
@@ -9,33 +9,9 @@ import (
 	"strings"
 )
 
-//"fmt"
-//"sync"
-//"time"
-
-/*func one (wg *sync.WaitGroup)  {
-	defer wg.Done()
-	fmt.Println("hola")
-}
-func two (wg *sync.WaitGroup)  {
-	defer wg.Done()
-	fmt.Println("ni hao")
-}
-func three (wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("hello")
-}
-*/
 
 func main() {
 
-	/*var wg sync.WaitGroup
-	wg.Add(3)
-	go one(&wg)
-	go two(&wg)
-	go three(&wg)
-	//
-	wg.Wait()*/
 
 	//set the flags
 	msg := flag.String("msg", "Howdy, stranger!", "the message to display")
