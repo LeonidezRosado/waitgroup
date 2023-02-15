1990c1d51c91ae421b2e0a1f24485edb5c308f26
Leonidez rosado <2020152397@ub.edu.bz>
2023-02-15 13:12:14 -0600

    flag5

--- main.go
+++ main.go
@@ -16,15 +16,21 @@ func main() {
 	//set the flags
 	msg := flag.String("msg", "Howdy, stranger!", "the message to display")
 	num := flag.Int("num",1 , "how many time to display the message")
-	caps := flag.Bool("caps", false,"should the string print in caps")
+	caps := flag.Bool("U", false,"specific whether to uppercase the string(true or false)")
 	flag.Parse()
 	//check if the user set the flag
 	//fmt.Println(*msg)
 	//fmt.Println(*num)
+
+
+	//when the user issues a flag reverse the string 
+	
+	//check if it should be uppercase before printing it  
 	if *caps {
 		*msg = strings.ToUpper(*msg)
 	}
-	
+
+	//print the s string 
 	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
 	}
