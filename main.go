730761337d0331c75810a61a4cc8d09b40595087
Leonidez rosado <2020152397@ub.edu.bz>
2023-02-15 13:33:49 -0600

    reverse flag

--- main.go
+++ main.go
@@ -17,17 +17,28 @@ func main() {
 	msg := flag.String("msg", "Howdy, stranger!", "the message to display")
 	num := flag.Int("num",1 , "how many time to display the message")
 	caps := flag.Bool("U", false,"specific whether to uppercase the string(true or false)")
+	reverse := flag.Bool("r", false, "reverse the string (true or false)")
 	flag.Parse()
 	//check if the user set the flag
 	//fmt.Println(*msg)
 	//fmt.Println(*num)
 
-
-	//when the user issues a flag reverse the string 
-	
 	//check if it should be uppercase before printing it  
 	if *caps {
 		*msg = strings.ToUpper(*msg)
+	}
+	//when the user issues a flag reverse the string 
+	//check if we should reverse the string 
+	if *reverse {
+		//create and empty string 
+		//2.iterate over the string that you want to reverse
+		var result string 
+		for _, value := range *msg {
+			result = string(value) + result
+	
+		}
+		//write the reverse string to *msg
+		*msg = result;
 	}
 
 	//print the s string 
